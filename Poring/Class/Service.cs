1c0a24868d018b37222a92f790809bbdf6626ef6
Leon Tsai <leontsai218@outlook.com>
2021-05-30 18:05:09 +0800

    add other service classes.

--- Poring/Class/Service.cs
+++ Poring/Class/Service.cs
@@ -1,15 +1,45 @@
 namespace Poring.Class
 {
-    public class Service1 : IService1
-    {
-        public int GetId()
-        {
-            return 9527;
-        }
-    }
-
     public interface IService1
     {
         int GetId();
+    }
+
+    public class Service1 : IService1
+    {
+        private readonly IService3 _service3;
+
+        public Service1(IService3 service3)
+        {
+            _service3 = service3;
+        }
+
+        public int GetId()
+        {
+            return 74 + _service3.GetSpecialNumber();
+        }
+    }
+
+
+    public interface IService2
+    {
+
+    }
+
+    public class Service2 : IService2
+    { }
+
+
+    public interface IService3
+    {
+        int GetSpecialNumber();
+    }
+
+    public class Service3 : IService3
+    {
+        public int GetSpecialNumber()
+        {
+            return 9453;
+        }
     }
 }
